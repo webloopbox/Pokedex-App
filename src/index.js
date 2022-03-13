39f71f848e7d1cfe51f2bed2ee9d7e00db5ab59f
Pawel <webloopbox@gmail.com>
2022-03-14 00:35:58 +0100

    organize scss files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/index.scss';
 import App from './App';
 import {configureStore} from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
